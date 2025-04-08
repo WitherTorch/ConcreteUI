71321c36c24c2c1894a502198b0968f4621be4eb
米茶 <new1271@outlook.com>
2025-04-08 13:44:10 +0800

    調整縮排

--- ConcreteUI.Test/Program.cs
+++ ConcreteUI.Test/Program.cs
@@ -1,6 +1,4 @@
 using System;
-using System.IO;
-using System.Runtime.ExceptionServices;
 using System.Windows.Forms;
 
 namespace ConcreteUI.Test
@@ -11,11 +9,10 @@ namespace ConcreteUI.Test
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        [HandleProcessCorruptedStateExceptions]
         static void Main()
         {
             ConcreteSettings.WindowMaterial = WindowMaterial.Acrylic;
-                Application.Run(new MainWindow(null));
+            Application.Run(new MainWindow(null));
         }
     }
 }
